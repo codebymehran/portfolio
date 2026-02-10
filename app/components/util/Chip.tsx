b96b983e1cdbfccf8aa8d95fdcc8aee19a5ff889
Mehran Khan <mehrankhan@Mehrans-MacBook-Pro.local>
2026-02-10 23:53:53 +1100

    under construction page

--- app/components/util/Chip.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const Chip = ({ children }: { children: string }) => {
-  return (
-    <span className="text-xs px-2 py-1 rounded bg-zinc-700">{children}</span>
-  );
-};
